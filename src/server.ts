1e9a2e0c0a51ed3ae62ccbd1088ffa42d1c29ea6
Jean Martins <jeanmrtns4@gmail.com>
2021-09-05 17:49:31 -0300

    Error midleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import 'reflect-metadata'
-import express from 'express'
+
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+
 import './database'
 import { router } from './routes'
 
@@ -8,6 +11,17 @@ const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    res.status(400).json({
+      error: error.message,
+    })
+  } else {
+    res.status(500).json({
+      error: 'Internal Server Error',
+    })
+  }
+})
 
 app.listen(3000, () => {
   console.log(`Running on PORT ${PORT}`)
